9b0a7c5e5dcee26081b7ae7cbe56537cadc93d76
Rade <rade.stevanovski@uklo.edu.mk>
2022-05-17 19:48:27 +0200

    initial

---
+++ app/src/main/java/com/puco/iptv/LoginActivity.java
@@ -0,0 +1,86 @@
+package com.puco.iptv;
+
+import android.content.Intent;
+//import android.support.annotation.Nullable;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView google_btn;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    private BeginSignInRequest signInRequest;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        google_btn = findViewById(R.id.signin_google);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        google_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignIn();
+            }
+        });
+    }
+
+    private void SignIn() {
+        Intent intent=gsc.getSignInIntent();
+        startActivityForResult(intent,100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==100){
+            Task<GoogleSignInAccount> task=GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                task.getResult(ApiException.class);
+                WlcmActivity();
+            } catch (ApiException e){
+                Toast.makeText(this,"Error", Toast.LENGTH_SHORT ).show();
+            }
+        }
+    }
+    private void WlcmActivity(){
+        finish();
+        Intent intent=new Intent(getApplicationContext(),WlcmActivity.class);
+        startActivity(intent);
+    }
+
+    private void Najava(){
+        signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                .build();
+    }
+}
---
+++ app/src/main/java/com/puco/iptv/WlcmActivity.java
@@ -0,0 +1,37 @@
+package com.puco.iptv;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+
+public class WlcmActivity extends Activity {
+
+    int a;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void IPTV(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void Kontakt(View view) {
+        LinearLayout ll = (LinearLayout) findViewById(R.id.infotextlayout);
+        ll.setVisibility(View.INVISIBLE);
+        LinearLayout ll1 = (LinearLayout) findViewById(R.id.kontakttextlayout);
+        ll1.setVisibility(View.VISIBLE);
+    }
+
+    public void INFOKLIK(View view) {
+        LinearLayout ll = (LinearLayout) findViewById(R.id.infotextlayout);
+        ll.setVisibility(View.VISIBLE);
+        LinearLayout ll1 = (LinearLayout) findViewById(R.id.kontakttextlayout);
+        ll1.setVisibility(View.GONE);
+    }
+}
